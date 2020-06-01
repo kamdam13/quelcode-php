efcf42b05af42fd4717b0239cf3d94af80c2a974
Kandam13 <h1rokmd13@gmail.com>
2020-06-01 11:54:03 +0900

    バブルソートの実装

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,8 +2,13 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+for ($i = 0; $i < count($array) - 1; $i++) {
+	for($j = 0; $j < count($array) - 1 - $i ; $j++)
+	 if($array[$j] > $array[$j+1]){
+		 $temp = $array[$j];
+		 $array[$j] = $array[$j+1];
+		 $array[$j+1] = $temp;
+	 }
 }
 // 修正はここまで
 
